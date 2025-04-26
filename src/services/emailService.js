54ca116d4496db6537b0e64096256e13bc0a3855
Juan Carlos Gómez <juan.gomez77@udea.edu.co>
2025-04-26 17:52:50 -0500

    configuracion email

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -9,7 +9,7 @@ export const sendEmail = async (templateParams) => {
     const response = await emailjs.send(
       import.meta.env.VITE_EMAILJS_SERVICE_ID,     // ID del servicio que creaste en el paso 2
       import.meta.env.VITE_EMAILJS_TEMPLATE_ID,    // ID de la plantilla que creaste en el paso 3
-      templateParams       // Parámetros que pasas desde el componente
+      templateParams                               // Parámetros que pasas desde el componente
     );
     
     console.log('Email enviado correctamente:', response);
